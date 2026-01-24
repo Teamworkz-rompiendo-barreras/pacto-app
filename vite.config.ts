aee989f2d4c0389be854bc4fefd3da8e0393f462
Ester Pérez <esperi1@hotmail.es>
2026-01-24 19:57:17 +0100

    Fix: Force React singleton with aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   resolve: {
     dedupe: ['react', 'react-dom'],
+    alias: {
+      react: path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    },
   },
   plugins: [react()],
   build: {
     outDir: 'dist',
     sourcemap: false,
-    chunkSizeWarningLimit: 1000, // Aumentar límite a 1MB para evitar warnings en Vercel
+    chunkSizeWarningLimit: 1000,
   },
   optimizeDeps: {
     include: ['react', 'react-dom'],
